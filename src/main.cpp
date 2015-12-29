42a0482e8942fdead2798af63a8d0d6938c0a6c3
Oleg Sushkov <osushkov@gmail.com>
2015-12-29 20:36:59 +1100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cout << "hello world" << endl;
+  return 0;
+}
